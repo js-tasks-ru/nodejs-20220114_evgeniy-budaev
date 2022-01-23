f8cc86796a0040e31584489a15d95521c6c095a7
Evgeniy Budaev <budaev.e@gmail.com>
2022-01-23 11:35:47 +0300

    03-streams/01-limit-size-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.encoding = options.encoding;
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size += chunk.byteLength;
+    if(this.size <= this.limit){
+      const out = chunk.toString(this.encoding);
+      callback(null, out);
+    } else {
+      callback(new LimitExceededError());
+    }
   }
 }
 
